625e2b2a49a88e7b596ef1c6e09099352e4a1a56
gilban-faresy <muhammadikmalgilbanfaresy@gmail.com>
2024-12-11 08:11:49 +0700

    Portofolio

--- src/components/Portofolio.jsx
+++ src/components/Portofolio.jsx
@@ -81,6 +81,19 @@ export default function Portofolio(){
                     </div>
                 </div>
             </div>
+            <div className="w-full">
+                <div className=" bg-white h-full w-full justify-center items-center flex rounded-lg overflow-hidden p-5 relative group">
+                    <img src="/images/projek0.png" alt="" />
+                    <div className="absolute p-5 bg-primary w-full h-full flex items-center opacity-0 group-hover:opacity-100 transition">
+                        <div className=" text-center w-full text-white mt-[-60px] group-hover:mt-0 transition-all">
+                            <div className="bg-white rounded-full px-5 inline-block mb-2">
+                                 <h4 className="text-gradient">WebBunga MTK</h4>
+                            </div>
+                                 <p>ini merupakan projek tugas untuk membuat website Kalkulator Bunga</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
         </div>
